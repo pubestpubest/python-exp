ee9d82d33ec68d03164e9a702b5dbad5ace7c8cd
pubest <110964000+pubestpubest@users.noreply.github.com>
2025-02-04 14:57:41 +0700

    Add explanation of magic methods in `magic_methods.py`
    * Add a brief introduction to magic methods and their purpose.
    * Add explanations for the following magic methods: `__init__`, `__str__`, `__repr__`, `__len__`, `__eq__`, and `__add__`.

--- magic_methods.py
+++ magic_methods.py
@@ -1,5 +1,7 @@
 # Magic Methods in Python
 
+# Magic methods, also known as dunder methods (short for "double underscore"), are special methods in Python that start and end with double underscores. They allow you to define the behavior of your objects for built-in operations such as addition, subtraction, string representation, and more. By implementing magic methods, you can make your custom classes behave like built-in types.
+
 # Defining a class with magic methods
 class Book:
     def __init__(self, title, author, pages):
@@ -36,3 +38,12 @@ print(repr(book1))
 print(len(book1))
 print(book1 == book2)
 print(book1 + book2)
+
+# Explanation of magic methods
+
+# __init__: This method is called when an instance of the class is created. It initializes the object's attributes.
+# __str__: This method is called by the str() function and the print statement to get a string representation of the object.
+# __repr__: This method is called by the repr() function and is used to get a string representation of the object that can be used to recreate the object.
+# __len__: This method is called by the len() function to get the length of the object.
+# __eq__: This method is called by the == operator to compare two objects for equality.
+# __add__: This method is called by the + operator to add two objects together.
